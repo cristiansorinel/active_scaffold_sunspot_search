5b7a33336f45c6d7cb264756467f3285f89c9ba0
Neatu Cristian <cristian.sorinel@gmail.com>
2011-02-25 18:43:29 +0200

    Code refactor, error handling

--- lib/active_scaffold/actions/sunspot_search.rb
+++ lib/active_scaffold/actions/sunspot_search.rb
@@ -27,43 +27,48 @@ module ActiveScaffold::Actions
     def do_sunspot_search
       query = sunspot_search_params.to_s.strip rescue ''
       unless query.empty?        
-        columns = active_scaffold_config.sunspot_search.columns        
-        #text_search = active_scaffold_config.sunspot_search.text_search
-        #sunspot_search_conditions = self.class.create_conditions_for_columns(query.split(' '), columns, text_search)
-        #self.active_scaffold_conditions = merge_conditions(self.active_scaffold_conditions, sunspot_search_conditions)
-        @filtered = true #!sunspot_search_conditions.blank?
+        columns = active_scaffold_config.sunspot_search.columns
+        condition = sunspot_solr_condition(query)
+        if @error.blank?
+          self.active_scaffold_conditions = merge_conditions(self.active_scaffold_conditions, condition)
+          @filtered = true
 
-        includes_for_search_columns = columns.collect{ |column| column.includes}.flatten.uniq.compact
-        self.active_scaffold_includes.concat includes_for_search_columns
+          includes_for_search_columns = columns.collect{ |column| column.includes}.flatten.uniq.compact
+          self.active_scaffold_includes.concat includes_for_search_columns
 
-        active_scaffold_config.list.user.page = nil
+          active_scaffold_config.list.user.page = nil
+        else
+          flash.now[:error] = @error
+        end
       end
     end
 
-    def conditions_for_collection      
-      search_condition = session["as:utilisation/documents"][:sunspot_search]
-      if search_condition.blank?
-        return []
-      end
-      
+    def sunspot_solr_condition(query)
       begin
         search = Document.search() do
-          keywords(search_condition)
+          keywords(query)
         end
-      rescue Errno::ECONNREFUSED => e        
-        return []
+      rescue Errno::ECONNREFUSED => e
+        @error = 'Documents search service is down!'
+        puts @error
+        return [] # :TODO - return error to interface
       end
-
+      
       results = search.results
-      cond = results.map &:id
-
-      model_table = active_scaffold_config.model.table_name
-      model_primary_key = active_scaffold_config.model.primary_key
-
-      results.nil? ? [] : ["#{model_table}.#{model_primary_key} IN (?)", cond]
+      results.nil? ? [] : build_database_search_condition(results.map(&:id))
     end
 
-    private
+    private    
+    # Builds search condition for database query    
+    # * return an array like [documents.id = 1] if solr return only 1 `id` or
+    # * return an array like [documents.id IN (1,2,3)] if solr return only more ids
+    def build_database_search_condition(ids)
+      operator = (ids.count == 1) ? '= ?' : ' IN (?)'
+      table = active_scaffold_config.model.table_name
+      primary_key = active_scaffold_config.model.primary_key
+      ["#{table}.#{primary_key} #{operator}", ids]
+    end
+
     def sunspot_search_authorized_filter
       link = active_scaffold_config.sunspot_search.link || active_scaffold_config.sunspot_search.class.link
       raise ActiveScaffold::ActionNotAllowed unless self.send(link.security_method)
